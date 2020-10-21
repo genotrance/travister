18e3e83b1fcf1129f8768ff0c00d7d4b898fa18f
Ganesh Viswanathan <dev@genotrance.com>
2020-10-21 09:50:12 -0500

    Test

--- test.sh
+++ test.sh
@@ -66,7 +66,7 @@ if [[ "$TRAVIS_CPU_ARCH" == "amd64" ]]
       "${NIM_SEMVER[0]}" -gt 1 \
     ]]
   then
-    gclone dom96/choosenim
+    gcloneBranch genotrance/choosenim misc
     cd choosenim
     nimble install -d -y  # Cannot nimble develop binary package
     nimble test
